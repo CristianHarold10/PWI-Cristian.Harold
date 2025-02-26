ba58e581b991b7b2897171fc0c749c82a9b281a9
cristian harold <cristian.harold100@gmail.com>
2025-02-25 22:26:54 -0300

    arquivos aula

---
+++ enviar_alteracao_para_repositorio_Executar_na_Pasta_do_Projeto.bat
@@ -0,0 +1,19 @@
+echo OFF
+
+echo --- Preparar aquivos para Subir ----
+
+git add .
+
+
+echo --- enviar ----
+
+git commit -m "Arquivos utilizados ou alterados na aula"
+
+echo --- Enviar para Nuvem
+
+git push
+
+echo --- Fim ---
+
+
+pause
